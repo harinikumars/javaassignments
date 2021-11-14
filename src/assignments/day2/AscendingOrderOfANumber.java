7511d361650a8463cc97a97d1d0bbf52d72bc2fd
Harini <harinikumar.s@gmail.com>
2021-11-14 13:46:17 +0530

    refactored for the 0 digit in the integer and added one more solution

--- src/assignments/day2/AscendingOrderOfANumber.java
+++ src/assignments/day2/AscendingOrderOfANumber.java
@@ -1,5 +1,6 @@
 package assignments.day2;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class AscendingOrderOfANumber {
@@ -8,6 +9,7 @@ public class AscendingOrderOfANumber {
         System.out.println("Please enter a number: ");
         int userInput = scanner.nextInt();
         ascendingOrderOfNumbers(userInput);
+        orderAscending(userInput);
     }
 
     static void ascendingOrderOfNumbers(int number) {
@@ -19,18 +21,45 @@ public class AscendingOrderOfANumber {
             System.out.println("It's only 1 digit. Please enter a string or a number of more than 1 character.");
         } else {
             int initialNumber = 0;
+            StringBuilder sb = new StringBuilder();
             for (int i = 0; i <= 9; i++) {
                 int inputNumber = number;
                 while (inputNumber > 0) {
                     int modulusResult = inputNumber % 10;
                     if (modulusResult == i) {
                         initialNumber *= 10;
-                        initialNumber+=modulusResult;
+                        initialNumber += modulusResult;
+                        sb.append(modulusResult);
                     }
                     inputNumber /= 10;
                 }
             }
-            System.out.println(initialNumber);
+            System.out.println(sb);
         }
+    }
+
+    static void orderAscending(int number) {
+        int length = String.valueOf(number).length();
+        int userInputNumber = number;
+        int[] numberArray = new int[length];
+        for (int i = 0; i < length; i++) {
+            numberArray[i] = userInputNumber%10;
+            userInputNumber=userInputNumber/10;
+        }
+        int init = 0;
+        for (int j = 0; j < numberArray.length; j++) {
+            for (int k = j + 1; k < numberArray.length; k++) {
+                if (numberArray[k] < numberArray[j]) {
+                    init = numberArray[j];
+                    numberArray[j] = numberArray[k];
+                    numberArray[k]=init;
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int a: numberArray){
+            sb.append(a);
+        }
+        System.out.println(sb);
     }
 }
