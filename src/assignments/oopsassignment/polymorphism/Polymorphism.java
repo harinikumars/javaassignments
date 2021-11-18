66eb206f4853e5a568bf510048961b4493ce25cb
Harini <harinikumar.s@gmail.com>
2021-11-18 12:08:12 +0530

    checking in polymorphism assignment

--- src/assignments/oopsassignment/polymorphism/Polymorphism.java
+++ src/assignments/oopsassignment/polymorphism/Polymorphism.java
@@ -1,31 +1,36 @@
 package assignments.oopsassignment.polymorphism;
 
-public class Polymorphism {
+import java.math.BigDecimal;
 
+public class Polymorphism {
+    //declaring the static variables
     static double number1;
     static double number2;
     static double number3;
 
+    //example of method overloading (same method name but arguments different) - compile time polymorphism
     static void largestOfGivenNumbers(double number1, double number2) {
         if (number1 > number2) {
-            System.out.println("The largest number is : " + number1);
+            System.out.println("The largest number is : " + new BigDecimal(number1).toPlainString());
         } else {
-            System.out.println("The largest number is : " + number2);
+            System.out.println("The largest number is : " + new BigDecimal(number2).toPlainString());
         }
     }
 
+    //example of method overloading (same method name but arguments different) - compile time polymorphism
     static void largestOfGivenNumbers(double number1, double number2, double number3) {
         if ( number1> number2 &&  number1> number3) {
-            System.out.println("The largest number is : " + number1);
+            System.out.println("The largest number is : " + new BigDecimal(number1).toPlainString());
         } else if (number2 >number1  && number2 > number3) {
-            System.out.println("The largest number is : " + number2);
+            System.out.println("The largest number is : " + new BigDecimal(number2).toPlainString());
         } else {
-            System.out.println("The largest number is : " + number3);
+            System.out.println("The largest number is : " + new BigDecimal(number3).toPlainString());
         }
     }
 
+    //example of method overriding (same method name and arguments but child class behaviour will be different) - runtime polymorphism
     void operatorOverloading(String input1, String input2) {
         Double sum =  Double.parseDouble(input1) + Double.parseDouble(input2);
-        System.out.println("The sum is : " + sum);
+        System.out.println("The sum is : " + new BigDecimal(sum).toPlainString());
     }
 }
--- src/assignments/oopsassignment/polymorphism/PolymorphismExample.java
+++ src/assignments/oopsassignment/polymorphism/PolymorphismExample.java
@@ -1,20 +1,34 @@
 package assignments.oopsassignment.polymorphism;
 
-public class PolymorphismExample extends Polymorphism{
+//inheriting the parent class
+public class PolymorphismExample extends Polymorphism {
     public static void main(String[] args) {
 
+        //initialising variables
         String input1 = "Hi!";
         String input2 = "There.";
-        number1=2;
-        number2=3;
-        number3=4;
+        number1=789;
+        number2=356789;
+        number3=43676845;
+
+        //checking method overloading (compile time polymorphism) - to find largest of 2 numbers
         largestOfGivenNumbers(number1, number2);
+
+        //checking method overloading (compile time polymorphism) - to find largest of 3 numbers
         largestOfGivenNumbers(number1, number2, number3);
+
+        //creating objects of parent class and child class to check method overriding
         Polymorphism objOfChild = new PolymorphismExample();
         Polymorphism objOfParent = new Polymorphism();
+
+        //method overriding example - operator overloading for String input concatenation
         objOfChild.operatorOverloading(input1,input2);
-        objOfParent.operatorOverloading("3","4");
+
+        //method overriding example - operator overloading for double input concatenation
+        objOfParent.operatorOverloading("390878.567","421321");
     }
+
+    //child class overriding the parent class method for operator overloading which is an example of method overriding of runtime polymorphism
         void operatorOverloading(String input1, String input2){
             System.out.println("Concatenating strings : " +(input1+input2));
         }
